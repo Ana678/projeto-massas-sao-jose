bce0df4d6ae0941fd32b6d204dc506ad50ea1040
Ana Carolina <anacarolinaneta@hotmail.com>
2023-12-21 16:32:22 -0300

    fisrt commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import TelaInicial from './components/TelaInicial.vue';
+import TelaVideo from './components/TelaVideo.vue';
+</script>
+
+<template>
+    <TelaVideo/>
+    <TelaInicial/>
+</template>
+
+<style scoped>
+</style>
